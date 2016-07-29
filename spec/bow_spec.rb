41acdb850994fa081e1c2f8d8e039cbe80b351d6
Bran Liang <lby89757@hotmail.com>
2016-07-29 13:18:25 +0800

    stuckt

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,31 @@
 # Your code here
+require './lib/weapons/bow.rb'
+
+describe Bow do
+
+  let(:bow){ Bow.new }
+
+  describe '#initialize' do
+    it 'has 10 arrows by default' do
+      expect(bow.arrows).to eq(10)
+    end
+    it 'created with a specified number of arrows' do
+      new_bow = Bow.new(100)
+      expect(new_bow.arrows).to eq(100)
+    end
+  end
+  describe '#use' do
+    it 'reduce the arrows by 1 when used once' do
+      bow.use
+      expect(bow.arrows).to eq(9)
+      bow.use
+      expect(bow.arrows).to eq(8)
+    end
+    it 'throw an error when used with 0 arrow' do
+      10.times { bow.use }
+      expect(bow.arrows).to eq(0)
+      expect{ bow.use }.to raise_error
+    end
+  end
+
+end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,102 @@
 # Your code here
+require 'viking.rb'
+require 'pry-byebug'
+
+describe Viking do
+  let(:viking){ Viking.new }
+  let(:oleg){ Viking.new("Oleg") }
+  let(:bran){ Viking.new("Bran") }
+
+  describe '#initialize' do
+    it 'set up with a name' do
+      new_viking = Viking.new("Bran")
+      expect(new_viking.name).to eq("Bran")
+    end
+    it 'set up with a particular healthy' do
+      new_viking = Viking.new("Bran", 101)
+      expect(new_viking.health).to eq(101)
+    end
+    it 'can not over written the health value' do
+      new_viking = Viking.new("Bran", 110)
+      expect(new_viking.health).to eq(110)
+      expect{ new_viking.health = 120 }.to raise_error(NoMethodError)
+    end
+    it 'has no weapon when initialized' do
+      expect(viking.weapon).to be_nil
+    end
+  end
+  describe '#pick_up_weapon' do
+    it 'can pick up viking weapon' do
+      axe = instance_double("Weapon", :name)
+      allow(axe).to receive(:is_a?).with(Weapon).and_return(true)
+      viking.pick_up_weapon(axe)
+      expect(viking.weapon).to eq(axe)
+    end
+    it 'can not pick none viking weapon' do
+      none_viking_weapon = double
+      allow(none_viking_weapon).to receive(:is_a?).with(Weapon).and_return(false)
+      expect{ viking.pick_up_weapon(none_viking_weapon) }.to raise_error("Can't pick up that thing")
+    end
+    it 'replace the old weapon' do
+      arrow = instance_double("Weapon")
+      axe = instance_double("Weapon")
+      allow(arrow).to receive(:is_a?).with(Weapon).and_return(true)
+      allow(axe).to receive(:is_a?).with(Weapon).and_return(true)
+      viking.pick_up_weapon(arrow)
+      expect(viking.weapon).to eq(arrow)
+      viking.pick_up_weapon(axe)
+      expect(viking.weapon).to eq(axe)
+    end
+  end
+  describe '#drop_weapon' do
+    it 'leave the viking weaponless' do
+      axe = instance_double("Weapon")
+      allow(axe).to receive(:is_a?).with(Weapon).and_return(true)
+      viking.pick_up_weapon(axe)
+      expect(viking.weapon).to eq(axe)
+      viking.drop_weapon
+      expect(viking.weapon).to be_nil
+    end
+  end
+  describe '#receive_attack' do
+    it 'reduce the viking health by the specified amount' do
+      expect(viking.health).to eq(100)
+      viking.receive_attack(10)
+      expect(viking.health).to eq(90)
+    end
+    it 'calls the take_damage method' do
+      expect(viking).to receive(:take_damage)
+      viking.receive_attack(10)
+    end
+  end
+  describe '#attack' do
+    it 'cause the recipients health to drop' do
+      expect(oleg.health).to eq(100)
+      bran.attack(oleg)
+      expect(oleg.health).to be < 100
+    end
+    it 'call the take_damage method' do
+      allow(oleg).to receive(:take_damage).and_return(2.5)
+      expect(oleg).to receive(:take_damage)
+      bran.attack(oleg)
+    end
+    it 'call method damage_with_fists when attack without weapon' do
+      # expect(bran.weapon).to be_nil
+      allow(bran).to receive(:damage_with_fists).and_return(10)
+      expect(bran).to receive(:damage_with_fists)
+      bran.attack(oleg)
+    end
+    it 'with no weapon deals Fists multiplier times strength damage' do
+
+    end
+    it 'with a weapon runs damage_with_weapon' do
+      # binding.pry
+      # axe = instance_double("Weapon",name: "axe", use: 1)
+      weapon = double("weapon", use: 2)
+      expect(bran).to receive(:pick_up_weapon).with(weapon)
+      # expect(bran).to receive(:weapon).and_return(axe)
+      expect(bran).to receive(:damage_with_weapon)
+      bran.attack(oleg)
+    end
+  end
+end
